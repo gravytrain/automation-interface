7c0b0dd40ff4c166104ce7dfb10b55a5c77d8d26
Michael <git@b3ck.me>
2014-09-06 20:44:40 -0600

    Added a basic Temperature DataTable.

--- js/app/main-dashboard.js
+++
@@ -1,9 +0,0 @@
-require(["jquery", "tab"], function($, tab) {
-
-	// Dashboard Tabs
-	$('#realtimeTabs a:last').tab('show');
-	$('#realtimeTabs a').click(function(e) {
-		e.preventDefault();
-		$(this).tab('show');
-	});
-});
---
+++ js/app/main-thermometer.js
@@ -0,0 +1,5 @@
+require(["jquery", "temp-table"], function($, tempTable) {
+
+   tempTable.displayEventDetails('#historic-s1', 'DrWahlBBQ', 's1');
+   tempTable.displayEventDetails('#historic-s2', 'DrWahlBBQ', 's2');
+});
