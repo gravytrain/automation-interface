6ea6c86abffa3205bb7ce9fb10d2a90de00c3ad8
Michael <git@b3ck.me>
2014-09-15 22:47:05 -0600

    Committing suicide?

--- js/configuration.js
+++ js/configuration.js
@@ -3,9 +3,6 @@ define({
 	version : "Insert Version Here",
 
 	webServiceSettings : {
-		"" : {
-			"MongoPimometer" : "http://mongo.drwahl.me:28017/pi_mometer/pi_collection/"
-		},
 		"localhost" : {
 			"MongoPimometer" : "http://localhost:28017/pi_mometer/pi_collection/"
 		}
